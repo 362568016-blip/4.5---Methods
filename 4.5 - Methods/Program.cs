a3f57a9f08558ea85be43bcfc16944b46a07bae5
362568016 <362568016@2260I-L29-025>
2026-09-22 13:18:41 -0400

    Add project files.

---
+++ 4.5 - Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿public static void ASIIart()
+{
+    Console.WriteLine("Press ENTER for ASII Art.");
+    Console.ReadLine();
+    ASIIart(3);
+    { 
+        Console.WriteLine("  __    __\r\no-''))_____\\\\\r\n\"--__/ * * * )\r\nc_c__/-c____/\r\n");
+        Console.WriteLine("                                  .-.\r\n     (___________________________()6 `-,\r\n     (   ______________________   /''\"`\r\n     //\\\\                      //\\\\\r\njgs  \"\" \"\"                     \"\" \"\"\r\n");
+        Console.WriteLine(",-.___,-.\r\n\\_/_ _\\_/\r\n  )O_O(\r\n { (_) }\r\n  `-^-'   hjw\r\n");
+    }
+    
+}
